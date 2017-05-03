e0d165c00ea58d6e5892797eda457224b00d32e7
Ankush Agrawal <ankagrawal@pdxinc.com>
2017-05-03 13:53:24 +0530

    Quiz 1 - Ankush

--- src/quiz/runner/ankush/QuizRunner.java
+++ src/quiz/runner/ankush/QuizRunner.java
@@ -1,7 +1,9 @@
 package quiz.runner.ankush;
 
+import java.util.Arrays;
 import java.util.List;
 
+import quiz.model.Ground;
 import quiz.model.QuizDataProvider;
 import quiz.model.Team;
 public class QuizRunner {
@@ -14,6 +16,35 @@ public class QuizRunner {
 
 		List<Team> teams = QuizDataProvider.getTeams();
 		System.out.println(teams);
+		
+		
+		
+		// 1
+		teams.forEach(team -> System.out.println(team.getName() + ":" + team.getHomeGround()));
+		
+		
+		
+		// 2
+		teams.sort((Team team1, Team team2) ->team1.getName().compareTo(team2.getName()));
+		
+		teams.forEach(team -> System.out.println(team.getName()));
+		
+		
+		
+		//3
+		teams.sort((Team team1, Team team2)->team2.getName().compareTo(team1.getName()));
+		
+		teams.forEach(team -> System.out.println(team.getName()));
+		
+		
+		
+		//4
+		List<Ground> homeGrounds = Arrays.asList(Ground.values());
+		
+		homeGrounds.sort((Ground ground1, Ground ground2)->ground1.name().compareTo(ground2.name()));
+		homeGrounds.forEach(ground -> {
+			System.out.println(ground.name());
+		});
 	}
 
 }
